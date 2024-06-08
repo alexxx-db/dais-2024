03b1565c788ec7003316662322f6cb6c058d066f
a0x8o <a0x8o>
2024-06-08 02:55:00 +0000

    FIX classroom setup 01

--- training/machine-learning-assoc/00_data-preparation-for-machine-learning/Solutions/Includes/Classroom-Setup-01.py
+++ training/machine-learning-assoc/00_data-preparation-for-machine-learning/Solutions/Includes/Classroom-Setup-01.py
@@ -3,7 +3,10 @@
 
 # COMMAND ----------
 
-DA = DBAcademyHelper(course_config, lesson_config)  # Create the DA object
+DA = DBAcademyHelper(course_config, lesson_config) # Create the DA object
+
+# COMMAND ----------
+
 DA.reset_lesson()                                   # Reset the lesson to a clean state
 DA.init()                                           # Performs basic intialization including creating schemas and catalogs
 DA.conclude_setup()                                 # Finalizes the state and prints the config for the student
