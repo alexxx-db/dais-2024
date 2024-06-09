ba23dc07a1b4876bb046717f25a1c55322dfeaee
a0x8o <a0x8o>
2024-06-09 16:31:09 +0000

    RUNME for dbdemos

---
+++ dbdemos/RUNME.py
@@ -0,0 +1,76 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+
+# COMMAND ----------
+
+dbdemos.help()
+
+# COMMAND ----------
+
+dbdemos.list_demos()
+
+# COMMAND ----------
+
+dbdemos.install('lakehouse-hls-readmission')
+
+# COMMAND ----------
+
+dbdemos.install('llm-rag-chatbot')
+
+# COMMAND ----------
+
+dbdemos.install('computer-vision-pcb')
+
+
+# COMMAND ----------
+
+dbdemos.install('llm-fine-tuning')
+
+# COMMAND ----------
+
+dbdemos.install('feature-store')
+
+
+# COMMAND ----------
+
+dbdemos.install('pandas-on-spark')
+
+# COMMAND ----------
+
+dbdemos.install('mlops-end2end')
+
+
+# COMMAND ----------
+
+dbdemos.install('sql-ai-functions')
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+dbdemos.install('streaming-sessionization')
+
+# COMMAND ----------
+
+dbdemos.install('cdc-pipeline')
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+dbdemos.install('dlt-cdc')
+
+
+# COMMAND ----------
+
+dbdemos.install('auto-loader')
+
